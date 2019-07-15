92d13cade9b01005c3f3d222f7685ca3a18967e9
Glen OLLIVIER <glen.ollivier@gmail.com>
2019-07-15 12:28:31 +0200

    Correction carrousel ng-bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,4 +28,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'collegues-front';
+  
+  images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,5 +16,4 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
 }
