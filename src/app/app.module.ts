43c7501c90d46548a1c2059c389baca06e9f39e6
Glen OLLIVIER <glen.ollivier@gmail.com>
2019-07-15 16:42:57 +0200

    Bugfix supression de C1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
-import { C1Component } from './c1/c1.component';
 import { CollegueComponent } from './collegue/collegue.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    C1Component,
     CollegueComponent
   ],
   imports: [
