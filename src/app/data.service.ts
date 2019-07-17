92a5ab6b128bf993120766aed84d44fdf4893131
Glen OLLIVIER <glen.ollivier@gmail.com>
2019-07-17 12:03:22 +0200

    Ajout d'un cache pour les collègues déja recherchés

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { Collegue } from './models/Collegue';
 import {environment} from '../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { Observable, Subject } from 'rxjs';
+import { Observable, Subject, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,17 +12,22 @@ export class DataService {
 
   BACKEND_URL = environment.backendUrl;
   COLLEGUE = new Collegue("-----","-----", "-----", new Date("0001-01-01"), "assets/images/keanu.png", "-----");
-  //collegues = new Map<string, Collegue>();
+  collegues = new Map<string, Collegue>();
   private subCollegueSelectionne = new Subject<Collegue>();
 
   constructor(private _http:HttpClient) { }
 
   rechercheParNom(nom: string): Observable<string[]> {
+    this.collegues.clear();
     return this._http.get<string[]>(this.BACKEND_URL + "/collegues?nom=" + nom);
   }
 
   rechercheParMatricule(matricule:string): Observable<Collegue> {
-    return this._http.get<Collegue>(this.BACKEND_URL + "/collegues/" + matricule);
+    if (this.collegues.has(matricule)) {
+      return of(this.collegues.get(matricule));
+    }
+    return this._http.get<Collegue>(this.BACKEND_URL + "/collegues/" + matricule)
+    .pipe(tap(collegue => this.collegues.set(collegue.matricule, collegue)));
   }
 
   recupererCollegueCourant():Collegue {
